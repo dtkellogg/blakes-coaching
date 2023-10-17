f3702e1f555719aae4aa387e14d773acd145a573
Toshi <dtkellogg10@gmail.com>
2023-10-17 09:56:24 -0400

    (WIP) MongoDB - setting up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
       colors: {
         primary: '#12234C',
         secondary: '#1778F2',
-        tertiary: 'white'
+        tertiary: '#ffffff'
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
