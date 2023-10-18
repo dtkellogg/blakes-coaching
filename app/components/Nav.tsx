96192743ee4ef591d5c38b26cd1e4f804163048e
Toshi <dtkellogg10@gmail.com>
2023-10-18 17:06:14 -0400

    Nav - active class added and working for all pages

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,7 +1,18 @@
+'use client'
+import { usePathname } from 'next/navigation'
+
 import Image from "next/image";
 import NavLink from "next/link";
 
 export default function Nav() {
+  const pathname = usePathname()
+  const isActive = (path: string) => {
+    console.log(`pathname: ${pathname.split('/')}`)
+    return pathname?.split('/')[1] === path
+  }
+
+  console.log(isActive('tasks'))
+
   return (
     <div className="flex items-center justify-between pr-10 margin-auto">
       <NavLink href="/">
@@ -15,13 +26,13 @@ export default function Nav() {
         />
       </NavLink>
       <section className="space-x-10">
-        <NavLink href="/tasks" className="hover:text-secondary transition ease-in-out">
+        <NavLink href="/tasks" className={`hover:text-secondary transition ease-in-out ${(isActive('tasks') || isActive('createActionItem') || isActive('editActionItem')) && 'underline decoration-secondary underline-offset-4'}`}>
           Tasks
         </NavLink>
-        <NavLink href="/video" className="hover:text-secondary transition ease-in-out">
+        <NavLink href="/video" className={`hover:text-secondary transition ease-in-out ${isActive('video') && 'underline decoration-secondary underline-offset-4'}`}>
           Video
         </NavLink>
-        <NavLink href="/" className="hover:text-secondary transition ease-in-out">
+        <NavLink href="/" className={`hover:text-secondary transition ease-in-out ${isActive('')}`}>
           Sign In
         </NavLink>
       </section>
