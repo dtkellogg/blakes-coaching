65af53a64a554f8d2f9b81463e370449d9325dad
Toshi <dtkellogg10@gmail.com>
2023-10-18 21:19:49 -0400

    Video page - added Coming Soon text

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -2,6 +2,9 @@ export default function Video() {
   return (
     <div className="">
       <h1 className="header-primary">Video</h1>
+      <div className="mt-[25vh] text-center text-xl">
+        <span>Coming soon...</span>
+      </div>
     </div>
   )
 }
