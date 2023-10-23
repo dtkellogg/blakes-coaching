cf7545e70850a745cb63b80806c07637728912c8
Toshi <dtkellogg10@gmail.com>
2023-10-23 19:50:58 -0400

    Models - Action Item model updated to include timestamps

--- app/models/actionItem.tsx
+++ app/models/actionItem.tsx
@@ -1,29 +1,32 @@
 import mongoose, { Schema } from "mongoose";
 
-const actionItemSchema = new Schema({
-  title: {
-    type: String,
-    required: [true, "Title is required."],
-    trim: true,
-    minLength: [2, "Title must be larger than 2 characters"],
-    maxLength: [50, "Title must be less than 50 characters"],
+const actionItemSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required."],
+      trim: true,
+      minLength: [2, "Title must be larger than 2 characters"],
+      maxLength: [50, "Title must be less than 50 characters"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required."],
+      trim: true,
+      minLength: [2, "Description must be larger than 2 characters"],
+      maxLength: [1000, "Description must be less than 1000 characters"],
+    },
+    deadline: {
+      type: Date,
+      required: [true, "Deadline is required."],
+    },
+    completed: {
+      type: Boolean,
+      default: false
+    }
   },
-  description: {
-    type: String,
-    required: [true, "Description is required."],
-    trim: true,
-    minLength: [2, "Description must be larger than 2 characters"],
-    maxLength: [1000, "Description must be less than 1000 characters"],
-  },
-  deadline: {
-    type: Date,
-    required: [true, "Deadline is required."],
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-});
+  { timestamps: true }
+);
 
 const ActionItem =
   mongoose.models.ActionItem || mongoose.model("ActionItem", actionItemSchema);
