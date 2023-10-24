78189ba5deb3d775e798320aece9223a9e5b429c
Toshi <dtkellogg10@gmail.com>
2023-10-24 15:24:08 -0400

    (WIP) Action Items Table V2

---
+++ app/components/ActionItemsTable2.tsx
@@ -0,0 +1,50 @@
+import { PencilSquareIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import DeleteActionItemButton from "./DeleteActionItemButton";
+import Tooltip from "./Tooltip";
+import CompletedActionItemButton from "./CompletedActionItemButton";
+
+export default async function ActionItemsTable2({ actionItems }) {
+
+  return (
+    <table className="table-fixed divide-y divide-gray-200">
+      {/* <caption className="caption-bottom text-quaternary">
+        Table 1: All Action Items sorted by deadline.
+      </caption> */}
+      <thead>
+        <tr className="bg-gray-400 text-black">
+          <th className="">Title</th>
+          <th className="" style={{width: '145px'}}>Deadline</th>
+          <th className="">Description</th>
+          <th className="" style={{width: '100px'}}>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {actionItems && actionItems
+          .sort((a,b) => new Date(a.deadline) - new Date(b.deadline))
+          .map(({ _id, title, deadline, description, completed }: any, i: number) => (
+          <tr key={i} className="bg-gray-100 text-black" style={{height: '100%'}}>
+            <td className="border-y-2 border-slate-700"><Tooltip message={title}><span className="line-clamp-1">{title}</span></Tooltip></td>
+            <td className="border-y-2 border-slate-700">{new Date(`${deadline}`).toUTCString().split(" 00:")[0]}</td>
+            <td className="border-y-2 border-slate-700"><Tooltip message={description}><span className="line-clamp-1">{description}</span></Tooltip></td>
+            <td className="border-y-2 border-slate-700" style={{height: '100%'}}>
+              <div className="flex">
+                <Tooltip message={"Delete"}>
+                  <DeleteActionItemButton id={_id} />
+                </Tooltip>
+                <Link href={`/tasks/editActionItem/${_id}?title=${title}&deadline=${deadline}&description=${description}`}>
+                  <Tooltip message={"Edit"}>
+                    <PencilSquareIcon className="h-8 w-8 cursor-pointer text-green-600" />
+                  </Tooltip>
+                </Link>
+                <Tooltip message={completed ? "Mark as Incomplete" : "Mark as Complete"}>
+                  <CompletedActionItemButton id={_id} title={title} deadline={deadline} description={description} completed={completed} />
+                </Tooltip>
+              </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 // import ActionItemsList from '@/app/components/ActionItemsList'
 import ActionItemsTable from '@/app/components/ActionItemsTable'
+import ActionItemsTable2 from '@/app/components/ActionItemsTable2'
 import Calendar from '@/app/components/Calendar'
 
 const getActionItems = async () => {
@@ -27,6 +28,9 @@ export default async function Tasks() {
     <main className="px-60 pb-4 mb-auto">
       <h1 className="header-primary mb-4">Tasks</h1>
       {/* <ActionItemsList /> */}
+      <div className="overflow-hidden border border-black rounded-lg mb-12">
+        <ActionItemsTable2 actionItems={actionItems} />
+      </div>
       <ActionItemsTable actionItems={actionItems} />
       <Link href="/tasks/createActionItem">
         <button className="btn-primary my-10 w-full">
