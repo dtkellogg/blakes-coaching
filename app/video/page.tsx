140ce26d8f1ea033e1c0b18df551ceca436ba591
Toshi <dtkellogg10@gmail.com>
2023-10-24 10:18:59 -0400

    (WIP) Video Page - redirecting user to home page if not logged in

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -1,4 +1,17 @@
-export default function Video() {
+// import { getServerSession } from "next-auth";
+// import { redirect } from "next/navigation";
+// import { authOptions } from "../api/auth/[...nextauth]/route";
+
+export default async function Video() {
+  // const session = await getServerSession(authOptions);
+
+  // if(session === null) {
+  //   console.log('!!! session:', session)
+  //   redirect('/')
+  // }
+
+  // console.log('session:', session)
+
   return (
     <div className="">
       <h1 className="header-primary">Video</h1>
