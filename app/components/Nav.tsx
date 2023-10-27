960f13da0ce1c3489261e06efc64827a8f186ece
Toshi <dtkellogg10@gmail.com>
2023-10-27 12:33:57 -0400

    Nav - settings menu almost finished. Only issue is when it's open and you click on the gear icon

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -8,11 +8,15 @@ import Image from "next/image";
 import NavLink from "next/link";
 import DarkModeButton from './buttons/DarkModeButton';
 import Tooltip from './Tooltip';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useTheme } from "next-themes"
+
+
 
 export default function Nav() {
   const [showMenu, setShowMenu] = useState(false)
+  const menuRef = useRef(null)
+  const gearRef = useRef(null)
   const pathname = usePathname()
   const isActive = (path: string) => {
     // console.log(`pathname: ${pathname.split('/')}`)
@@ -28,6 +32,7 @@ export default function Nav() {
 
   const toggleMenu = () => {
     setShowMenu(!showMenu)
+    console.log('Menu', showMenu)
   }
 
   const toggleDarkMode = () => {
@@ -36,6 +41,31 @@ export default function Nav() {
       ? setTheme('light')
       : setTheme('dark')
   }
+
+  function useOutsideAlerter(ref) {
+    useEffect(() => {
+      /**
+       * Alert if clicked on outside of element
+       */
+      function handleClickOutside(event) {
+        // if (ref.current && !ref.current.contains(event.target) && !ref.current.contains(gearRef)) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setShowMenu(false)
+          // console.log("You clicked outside of me!");
+          // console.log(`showMenu`, showMenu)
+          // console.log('ref.current.contains(gearRef)', ref.current)
+        }
+      }
+      // Bind the event listener
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        // Unbind the event listener on clean up
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  }
+
+  useOutsideAlerter(menuRef)
 
   return (
     <div className="grid grid-cols-3 w-full text-primary dark:text-quaternary">
@@ -63,34 +93,39 @@ export default function Nav() {
           <Cog6ToothIcon
             className="h-8 w-8 cursor-pointer text-gray-900 dark:text-quaternary"
             onClick={() => (toggleMenu())}
+            ref={gearRef}
           />
         {/* </Tooltip> */}
-        <ul 
-          className={`${showMenu ? 'block' : 'hidden'} absolute top-20 flex flex-col justify-end border border-black rounded-lg p-2 space-y-3 z-[99] bg-white`}
-          // onBlur={() => (toggleMenu())}
-        >
-          {/* <li><DarkModeButton /></li> */}
-          <li className="hover:cursor-pointer">
-            <label className="relative flex justify-between items-center text-black hover:text-secondary hover:cursor-pointer">
-              <input type="checkbox" className="absolute left-1/2 -translate-x-1/2 w-full h-full peer appearance-none rounded-md" onClick={() => (toggleDarkMode())}/>
-              <span className="w-8 h-5 flex items-center flex-shrink-0 mr-4 p-1 bg-gray-300 rounded-full duration-300 ease-in-out peer-checked:bg-green-400 after:w-4 after:h-4 after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-2"></span>
-              Dark Mode
-            </label>
-          </li>
-          <li className="text-black text-end">
-            {session?.user
-              ? <button
-                  onClick={() => signOut()} 
-                  className={`hover:text-secondary transition ease-in-out ${isActive('')}`}
-                >
-                  Sign out
-                </button>
-              :  <NavLink href="/login" className={`hover:text-secondary transition ease-in-out ${isActive('')}`}>
-                  Sign In
-                </NavLink>
-            }
-          </li>
-        </ul>
+        {showMenu && (
+
+          <ul 
+            className={`absolute top-20 flex flex-col justify-end border border-black rounded-lg p-2 space-y-3 z-[99] bg-white`}
+            ref={menuRef}
+            // onBlur={() => (toggleMenu())}
+          >
+            {/* <li><DarkModeButton /></li> */}
+            <li className="hover:cursor-pointer">
+              <label className="relative flex justify-between items-center text-black hover:text-secondary hover:cursor-pointer">
+                <input type="checkbox" checked={currentTheme === "dark"} className="absolute left-1/2 -translate-x-1/2 w-full h-full peer appearance-none rounded-md" onClick={() => (toggleDarkMode())}/>
+                <span className="w-8 h-5 flex items-center flex-shrink-0 mr-4 p-1 bg-gray-300 rounded-full duration-300 ease-in-out peer-checked:bg-green-400 after:w-4 after:h-4 after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-2"></span>
+                Dark Mode
+              </label>
+            </li>
+            <li className="text-black text-end">
+              {session?.user
+                ? <button
+                    onClick={() => signOut()} 
+                    className={`hover:text-secondary transition ease-in-out ${isActive('')}`}
+                  >
+                    Sign out
+                  </button>
+                :  <NavLink href="/login" onClick={() => setShowMenu(false)} className={`hover:text-secondary transition ease-in-out ${isActive('')}`}>
+                    Sign In
+                  </NavLink>
+              }
+            </li>
+          </ul>
+        )}
       </section>
       
       {/* <section className="flex items-center justify-end space-x-10 mr-10">
